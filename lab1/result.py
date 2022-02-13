17d6d4322c2e31caaeff6bb325b504848d5d4174
st-a-novoseltcev <st.a.novoseltcev@gmail.com>
2022-02-13 15:13:18 +0000

    LAB-1 Refactoring: removing code duplication, rename variables, functions selection

--- lab1/result.py
+++ lab1/result.py
@@ -1,308 +1,243 @@
 import random
+from copy import deepcopy
+import numpy as np
 
-n1, m1, n2, m2, k, l = 0, 0, 0, 0, 0, 2
-print('Вас приветствует программа\n'
-      'быстрого перемножения матриц методом Штрассена\n')
-
-while n1 <= 0 or m1 <= 0:
-    n1, m1 = map(int, input('Введите размеры первой матрицы\n').split())
-
-while n2 <= 0 or m2 <= 0:
-    n2, m2 = map(int, input('Введите размеры второй матрицы\n').split())
-
-M1 = list()
-for i in range(n1):
-    M1.append([None] * m1)
-
-M2 = list()
-for i in range(n2):
-    M2.append([None] * m2)
-
-"""
-    Выбор способа заполнения и заполнение матриц
-"""
-
-while k < 1 or k > 2:
-    k = int(input('Выберите способ заполнения матриц\n'
-                  '1 - Вручную \n'
-                  '2 - Случайным образом\n'))
-
-match k:
-    case 1:
-        for i in range(n1):
-            for j in range(m1):
-                M1[i][j] = int(input())
-
-        for i in range(n2):
-            for j in range(m2):
-                M2[i][j] = int(input())
-
-        print('\nМатрица 1\n')
-        for i in range(n1):
-            for j in range(m1):
-                print(M1[i][j], end=' ')
-            print()
-
-        print('\nМатрица 2\n')
-        for i in range(n2):
-            for j in range(m2):
-                print(M2[i][j], end=' ')
-            print()
-
-    case 2:
-        for i in range(n1):
-            for j in range(m1):
-                M1[i][j] = random.randint(0, 10)
-        for i in range(n2):
-            for j in range(m2):
-                M2[i][j] = random.randint(0, 10)
-
-        print('\nМатрица 1\n')
-        for i in range(n1):
-            for j in range(m1):
-                print(M1[i][j], end=' ')
-            print()
-
-        print('\nМатрица 2\n')
-        for i in range(n2):
-            for j in range(m2):
-                print(M2[i][j], end=' ')
-            print()
-
-"""
-    Приведение матриц к требуемому размеру
-"""
-
-while l < n1 or l < n2 or l < m1 or l < m2:
-    l *= 2
-
-M3 = list()
-for i in range(l):
-    M3.append([0] * l)
-
-M4 = list()
-for i in range(l):
-    M4.append([0] * l)
-
-for i in range(n1):
-    for j in range(m1):
-        M3[i][j] = M1[i][j]
-
-for i in range(n2):
-    for j in range(m2):
-        M4[i][j] = M2[i][j]
-
-print('Приведенные матрицы\n')
-
-print('Матрица 1\n')
-for i in range(l):
-    for j in range(l):
-        print(M3[i][j], end=' ')
-    print()
-
-print('Матрица 2\n')
-for i in range(l):
-    for j in range(l):
-        print(M4[i][j], end=' ')
-    print()
-
-""" 
-    Разбиение матриц на подматрицы и их заполнение
-"""
-
-mat1 = list()
-for i in range(l // 2):
-    mat1.append(M3[i][:l // 2])
-
-mat2 = list()
-for i in range(l // 2):
-    mat2.append(M3[i][l // 2:])
-
-mat3 = list()
-for i in range(l // 2):
-    mat3.append(list())
-    for j in range(l // 2):
-        mat3[i].append(M3[l // 2 + i][j])
-
-mat4 = list()
-for i in range(l // 2):
-    mat4.append(list())
-    for j in range(l // 2):
-        mat4[i].append(M3[l // 2 + i][l // 2 + j])
-
-mat5 = list()
-for i in range(l // 2):
-    mat5.append(M4[i][:l // 2])
-
-mat6 = list()
-for i in range(l // 2):
-    mat6.append(M4[i][l // 2:])
-
-mat7 = list()
-for i in range(l // 2):
-    mat7.append(list())
-    for j in range(l // 2):
-        mat7[i].append(M4[l // 2 + i][j])
-
-mat8 = list()
-for i in range(l // 2):
-    mat8.append(list())
-    for j in range(l // 2):
-        mat8[i].append(M4[l // 2 + i][l // 2 + j])
-
-"""
-    Создание промежуточных матриц
-"""
-
-p1 = list()
-for i in range(l // 2):
-    p1.append([None] * (l // 2))
-
-p2 = list()
-for i in range(l // 2):
-    p2.append([None] * (l // 2))
-
-p3 = list()
-for i in range(l // 2):
-    p3.append([None] * (l // 2))
-
-p4 = list()
-for i in range(l // 2):
-    p4.append([None] * (l // 2))
-
-p5 = list()
-for i in range(l // 2):
-    p5.append([None] * (l // 2))
-
-p6 = list()
-for i in range(l // 2):
-    p6.append([None] * (l // 2))
-
-p7 = list()
-for i in range(l // 2):
-    p7.append([None] * (l // 2))
-
-"""
-    Вычисление значений промежуточных матриц
-"""
-
-for i in range(l // 2):
-    for j in range(l // 2):
-        p1[i][j] = 0
-        for z in range(l // 2):
-            p1[i][j] += (mat1[i][z] + mat4[i][z]) * (mat5[z][j] + mat8[z][j])
-
-        p2[i][j] = 0
-        for z in range(l // 2):
-            p1[i][j] += (mat3[i][z] + mat4[i][z]) * mat5[z][j]
-
-        p3[i][j] = 0
-        for z in range(l // 2):
-            p3[i][j] += mat1[i][z] * (mat4[i][z] -  mat8[z][j])
-
-        p4[i][j] = 0
-        for z in range(l // 2):
-            p4[i][j] += mat4[i][z] * (mat7[z][j] - mat5[z][j])
-
-        p5[i][j] = 0
-        for z in range(l // 2):
-            p5[i][j] += (mat1[i][z] + mat2[i][z]) * mat8[z][j]
-
-        p6[i][j] = 0
-        for z in range(l // 2):
-            p6[i][j] += (mat3[i][z] - mat1[i][z]) * (mat5[z][j] + mat6[z][j])
-
-        p7[i][j] = 0
-        for z in range(l // 2):
-            p7[i][j] += (mat2[i][z] - mat4[i][z]) * (mat7[z][j] + mat8[z][j])
-
-"""
-    Создание вспомогательных матриц
-"""
-
-mat9 = list()
-for i in range(l // 2):
-    mat9.append([None] * (l // 2))
-
-mat10 = list()
-for i in range(l // 2):
-    mat10.append([None] * (l // 2))
-
-mat11 = list()
-for i in range(l // 2):
-    mat11.append([None] * (l // 2))
-
-mat12 = list()
-for i in range(l // 2):
-    mat12.append([None] * (l // 2))
-
-"""
-    Подсчет значений вспомогательных матриц из промежуточных
-"""
-
-for i in range(l // 2):
-    for j in range(l // 2):
-        mat9[i][j] = p1[i][j] + p4[i][j] - p5[i][j] + p7[i][j]
-        mat10[i][j] = p3[i][j] + p5[i][j]
-        mat11[i][j] = p2[i][j] + p4[i][j]
-        mat12[i][j] = p1[i][j] - p2[i][j] + p3[i][j] + p6[i][j]
-
-"""
-    Создание результирующей матрицы
-"""
-
-M5 = list()
-for i in range(l):
-    M5.append([None] * l)
-
-"""
-    Занесение информации из вспомогательных матриц в результирующую
-"""
-
-for i in range(l // 2):
-    for j in range(l // 2):
-        M5[i][j] = mat9[i][j]
-        M5[i][j + l // 2] = mat10[i][j]
-        M5[i + l // 2][j] = mat11[i][j]
-        M5[i + l // 2][j + l // 2] = mat12[i][j]
-
-"""
-    Выравнивание границ результирующей матрицы
-"""
-
-x, f, s = 0, 100, 100
-for i in range(l):
-    x = 0
-    for j in range(l):
-        if M5[i][j] != 0:
-            x += 1
-            f = 100
-    if x == 0 and i < f:
-        f = i
-
-for i in range(l):
-    x = 0
-    for j in range(l):
-        if M5[i][j] != 0:
-            x += 1
-            s = 100
-    if x == 0 and i < s:
-        s = i
-
-M6 = list()
-for i in range(f):
-    M6.append(
-        M5[i][:s]
-    )
-
-"""
-    Вывод результирующей матрицы
-"""
-
-print('\nРезультирующая матрица\n')
-for i in range(f):
-    for j in range(s):
-        print(M6[i][j], end=' ')
-    print()
-
-input('\n')
+
+def input_int(l_boundary: int, r_boundary: int, msg: str):
+    result = None
+    while result < l_boundary or result > r_boundary:
+        result = int(input(msg))
+    return result
+
+
+def init_matrix(n_rows: int, n_columns: int, fill=None) -> list[list]:
+    result = list()
+    for _ in range(n_rows):
+        result.append([fill] * n_columns)
+    return result
+
+
+def get_matrix_size(matrix: list[list]) -> tuple[int, int]:
+    return len(matrix), len(matrix[0])
+
+
+def print_matrix(matrix: list[list]) -> None:
+    for row in matrix:
+        print(' '.join(map(str, row)))
+
+
+def _expand_rows_len(matrix: list[list], row_len: int, fill) -> None:
+    _, n_column = get_matrix_size(matrix)
+    print(f'{n_column=}')
+    for row in matrix:
+        row += [fill] * (row_len - n_column)
+
+
+def _expand_rows_count(matrix: list[list], row_count: int, fill) -> None:
+    _, row_len = get_matrix_size(matrix)
+    for _ in range(row_count - len(matrix)):
+        matrix.append([fill] * row_len)
+
+
+def expand_matrix(matrix: list[list], n_rows: int, n_columns: int, fill=None) -> list[list]:
+    new_matrix = deepcopy(matrix)
+    _expand_rows_len(new_matrix, n_columns, fill)
+    _expand_rows_count(new_matrix, n_rows, fill)
+    return new_matrix
+
+
+def input_matrix(n_rows: int, n_columns: int) -> list[list]:
+    result = list()
+    for _ in range(n_rows):
+        result.append(
+            list(int(input()) for _ in range(n_columns))
+        )
+    return result
+
+
+def randomize_matrix(n_rows: int, n_columns: int, interval: tuple[int, int]) -> list[list]:
+    result = list()
+    for _ in range(n_rows):
+        result.append(
+            list(random.randint(*interval) for _ in range(n_columns))
+        )
+    return result
+
+
+def input_matrix_size(msg=None):
+    n_rows = n_columns = -1
+    while n_rows <= 0 or n_columns <= 0:
+        n_rows, n_columns = map(int, input(msg).split())
+    return n_rows, n_columns
+
+
+def _split_matrix_to_half(matrix: list[list]) -> tuple[list[list], list[list]]:
+    _, n_columns = get_matrix_size(matrix)
+    left_matrix_half = list()
+    for row in matrix:
+        left_matrix_half.append(row[:n_columns // 2])
+    right_matrix_half = list()
+
+    for row in matrix:
+        right_matrix_half.append(row[n_columns // 2:])
+    return left_matrix_half, right_matrix_half
+
+
+def split_matrix_to_corners(matrix: list[list]) -> tuple[list[list], list[list], list[list], list[list]]:
+    n_rows, _ = get_matrix_size(matrix)
+    left_matrix_half, right_matrix_half = _split_matrix_to_half(matrix)
+    left_top_corner = left_matrix_half[:n_rows // 2]
+    left_bottom_corner = left_matrix_half[n_rows // 2:]
+    right_top_corner = right_matrix_half[:n_rows // 2]
+    right_bottom_corner = right_matrix_half[n_rows // 2:]
+    return left_top_corner, right_top_corner, left_bottom_corner, right_bottom_corner
+
+
+def _find_matrix_down_boundary(matrix: list[list]) -> int:
+    n_rows, n_columns = get_matrix_size(matrix)
+    result = 0
+    for row_num in range(n_rows - 1, -1, -1):
+        if matrix[row_num].count(0) != n_columns:
+            result = row_num
+            break
+    return result + 1
+
+
+def _find_matrix_right_boundary(matrix: list[list]) -> int:
+    n_rows, n_columns = get_matrix_size(matrix)
+    result = 0
+    for column_num in range(n_columns - 1, -1, -1):
+        column = [matrix[row_n][column_num] for row_n in range(n_rows)]
+        if column.count(0) != n_rows:
+            result = column_num
+            break
+    return result + 1
+
+
+def align_matrix(matrix: list[list[int]]) -> list[list]:
+    new_height = _find_matrix_down_boundary(matrix)
+    new_length = _find_matrix_right_boundary(matrix)
+    result = list()
+    for row in matrix[:new_height]:
+        result.append(row[:new_length])
+    return result
+
+
+def join_corners(
+        left_top_corner: list[list],
+        right_top_corner: list[list],
+        left_bottom_corner: list[list],
+        right_bottom_corner: list[list]
+) -> list[list]:
+    result = list()
+    for i in range(middle_l):
+        result.append(left_top_corner[i] + right_top_corner[i])
+    for i in range(middle_l):
+        result.append(left_bottom_corner[i] + right_bottom_corner[i])
+    return result
+
+
+if __name__ == '__main__':
+    print('Вас приветствует программа\n'
+          'быстрого перемножения матриц методом Штрассена\n')
+
+    n_rows_A, n_cols_A = input_matrix_size('Введите размеры первой матрицы\n')
+    n_rows_B, n_cols_B = input_matrix_size('Введите размеры второй матрицы\n')
+    fill_type = input_int(
+        l_boundary=1,
+        r_boundary=2,
+        msg='Выберите способ заполнения матриц\n'
+                        '1 - Вручную \n'
+                        '2 - Случайным образом\n')
+
+    """Заполнение матриц."""
+    match fill_type:
+        case 1:
+            A = input_matrix(n_rows_A, n_cols_A)
+            B = input_matrix(n_rows_B, n_cols_B)
+
+        case 2:
+            A = randomize_matrix(n_rows_A, n_cols_A, (1, 10))
+            B = randomize_matrix(n_rows_B, n_cols_B, (1, 10))
+
+        case _:
+            raise ValueError(f'{fill_type=} must be 1 or 2')
+
+    print('\nМатрица 1:\n')
+    print_matrix(A)
+
+    print('\nМатрица 2:\n')
+    print_matrix(B)
+
+    """Приведение матриц к требуемому размеру"""
+    expand_size = 2
+    while expand_size < max(n_rows_A, n_rows_B, n_cols_A, n_cols_B):
+        expand_size *= 2
+
+    expanded_A = expand_matrix(A, expand_size, expand_size, 0)
+    expanded_B = expand_matrix(B, expand_size, expand_size, 0)
+
+    print('Приведенные матрицы\n')
+    print('Матрица 1:\n')
+    print_matrix(expanded_A)
+
+    print('\nМатрица 2:\n')
+    print_matrix(expanded_B)
+
+    A11, A12, A21, A22 = split_matrix_to_corners(expanded_A)
+    B11, B12, B21, B22 = split_matrix_to_corners(expanded_B)
+
+    """Вычисление значений промежуточных матриц"""
+    middle_l = expand_size // 2
+    D = init_matrix(middle_l, middle_l, 0)
+    D1 = init_matrix(middle_l, middle_l, 0)
+    D2 = init_matrix(middle_l, middle_l, 0)
+    H1 = init_matrix(middle_l, middle_l, 0)
+    H2 = init_matrix(middle_l, middle_l, 0)
+    V1 = init_matrix(middle_l, middle_l, 0)
+    V2 = init_matrix(middle_l, middle_l, 0)
+
+    for i in range(middle_l):
+        for j in range(middle_l):
+            for z in range(middle_l):
+                D[i][j] += (A11[i][z] + A22[i][z]) * (B11[z][j] + B22[z][j])
+
+            for z in range(middle_l):
+                D1[i][j] += (A12[i][z] - A22[i][z]) * (B21[z][j] + B22[z][j])
+
+            for z in range(middle_l):
+                D2[i][j] += (A21[i][z] - A11[i][z]) * (B11[z][j] + B12[z][j])
+
+            for z in range(middle_l):
+                H1[i][j] += (A11[i][z] + A12[i][z]) * B22[z][j]
+
+            for z in range(middle_l):
+                H2[i][j] += (A21[i][z] + A22[i][z]) * B11[z][j]
+
+            for z in range(middle_l):
+                V1[i][j] += A22[i][z] * (B21[z][j] - B11[z][j])
+
+            for z in range(middle_l):
+                V2[i][j] += A11[i][z] * (B12[z][j] - B22[z][j])
+
+    """Подсчет значений вспомогательных матриц из промежуточных"""
+    R11 = init_matrix(middle_l, middle_l)
+    R12 = init_matrix(middle_l, middle_l)
+    R21 = init_matrix(middle_l, middle_l)
+    R22 = init_matrix(middle_l, middle_l)
+
+    for i in range(middle_l):
+        for j in range(middle_l):
+            R11[i][j] = D[i][j] + D1[i][j] - H1[i][j] + V1[i][j]
+            R12[i][j] = V2[i][j] + H1[i][j]
+            R21[i][j] = V1[i][j] + H2[i][j]
+            R22[i][j] = D[i][j] + D2[i][j] + V2[i][j] - H2[i][j]
+
+    preR = join_corners(R11, R12, R21, R22)
+    R = align_matrix(preR)
+
+    print('\nРезультирующая матрица\n')
+    print_matrix(R)
+
+    assert np.matrix(R) == np.matmul(np.matrix(expanded_A), np.matrix(expanded_B))
+    input('\n')
